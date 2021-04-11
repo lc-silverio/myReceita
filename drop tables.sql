fd0d4458dd005c790be16a0289ae4cd888fe2c01
Luís Silvério <oppositedeep@gmail.com>
2021-04-11 21:41:54 +0100

    database

---
+++ drop tables.sql
@@ -0,0 +1,15 @@
+drop TABLE paciente;
+
+drop table funcao;
+
+drop table categoria;
+
+drop table estado;
+
+drop table medicamento;
+
+drop table estabelecimento;
+
+drop table funcionario;
+
+drop table receita;
